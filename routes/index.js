a8d193949b7f63d8c239238e6df050818606110e
JunHong <jhong365@gmail.com>
2013-09-14 08:21:36 -0700

    Add request

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 
-/*
- * GET home page.
- */
+var config = require('./config')('prod');
+var loginUrl = config.sbp.host + 'account/login';
+var request = require('request');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res0){
+	var getUserUrl = config.sbp.host + 'account/list';
+	request({
+		url : getUserUrl,
+		method : 'GET'
+	}, function(err, res, body) {
+		if (!err && res.statusCode == 200) {
+		var users = JSON.parse(body);
+		res0.render('index', { users: users });	
+	} else {			
+			res0.render('index', { users: [{"email":getUserUrl}] });
+		}
+	});
 };
