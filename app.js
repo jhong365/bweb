35b1c90651dd7f15172e82681dc592ea356ca893
JunHong <jhong365@gmail.com>
2013-09-24 09:48:57 -0700

    Add s3 upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var http = require('http');
 var path = require('path');
 var passport = require("passport");
 var config = require('./config/config').Config;
+var AWS = require('aws-sdk');
 require('./config/passport')(passport, config);
 
 var app = express();
