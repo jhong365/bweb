d6004fccce8a21d06246c541dc1b95b4bf99fc45
JunHong <jhong365@gmail.com>
2013-09-24 09:48:57 -0700

    Add s3 upload

--- routes/gig.js
+++ routes/gig.js
@@ -3,25 +3,43 @@ var request = require('request');
 exports.get = function(req, res){
 	res.render('gig');
  };
- 
+
+var fs = require('fs');
 var config = require('../config/config').Config;
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config/aws.json');
+
+function uploadToS3(key, path){
+	var data = {Key: key , Body: fs.createReadStream(path), ACL:"public-read"};
+	var s3bucket = new AWS.S3({params: {Bucket: 'boostaa'}});
+	s3bucket.putObject(data, function(err, data) {
+	    if (err) {
+	      console.log("Error uploading data: ", err);
+	    } else {
+	      console.log("Successfully uploaded data to S3: " + key);
+	    }
+	 });
+}
  
 exports.post = function(req, resp){
-	
-	var fs = require('fs');
-	
-	console.log("1111");
 	var r = request.post(config.sbp.host + 'gig');
 	var form = r.form();
 	form.append("userId", "1");
 	form.append("title", req.body.title);
 	form.append("description", req.body.description);
 	form.append("tags", req.body.tags);
-	var photos = req.files.photos;
-	for(var i in photos){
-		form.append('photos', fs.createReadStream(photos[i].path));
-	}
 	
+	var photos = req.files.photos;
+	
+	for(var i in photos){
+		if(photos[i].name){
+			var s3key = new Date().getTime() + photos[i].name;
+			uploadToS3(s3key, photos[i].path);
+			form.append("photos", s3key);
+		}
+	}
+
+	resp.render('success');
 	console.log(form);
 	form.submit(config.sbp.host + 'gig', function(err, res){
 		if(err){
@@ -31,7 +49,6 @@ exports.post = function(req, resp){
 			resp.render('success');
 		}
 	});
-
 
 	/* 
 	var data = new FormData();
