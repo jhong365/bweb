6e4a40521d24572d2f2a59b945309f4d82fd5a22
cabbagetang <cabbagetang@gmail.com>
2013-09-30 22:43:51 -0700

    Add profile link

--- routes/profile.js
+++ routes/profile.js
@@ -2,8 +2,15 @@
 var config = require('../config/config').Config;
 var loginUrl = config.sbp.host + 'account/login';
 var request = require('request');
+var qs = require('querystring');
 
-exports.profile = function(req, resp){
+
+exports.profile = function(req, res){
 	var data = {};
-	resp.render('profile', data);
-};
+	if (req.isAuthenticated()) {
+		data.user = req.user;
+	} else {
+		res.redirect('/login?' + qs.stringify({'redirect': req.url}));
+	}
+	res.render('profile', data);
+ };
