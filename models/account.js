120561c495b1e5893d58b9b03d4f92f7f9341901
cynosureabu <cynosureabu@gmail.com>
2013-10-07 23:45:39 -0700

    account model with orm2

--- models/account.js
+++ models/account.js
@@ -1,8 +1,18 @@
-var db = require('../settings').db;
-var Account = db.define('account',{
-	email    : { type: "text", size: 255, required: true },
-	password : { type: "text", size: 255, required: true },
-	role     : { type: "number", rational: false, size: 1}
-});
+module.exports = function(db, models, next) {
+	console.log('create account model');
+	
+	models.account = db.define("account", {
+		email : String,
+		password : String,
+		role: { type: "enum", values: ["business", "promoter", "both"]},
+		created : Date,
+		updated : Date
+	});
+	
+	models.account.findByEmail = function(email, callback) {
+		  this.find({ email: email }, callback);
+	};
+	
+    return next(); 
+};
 
-module.exports = Account;
